dcdafdee2243b9eb729e88e1a8464bd5b6287179
Eric Mill <eric@konklone.com>
2013-10-28 15:09:40 -0400

    Handle the documented arguments

--- inspectors/usps.py
+++ inspectors/usps.py
@@ -17,12 +17,35 @@ from pyquery import PyQuery
 #          defaults to None (will fetch all reports)
 
 def run(options):
-  url = "http://www.uspsoig.gov/document-library"
+  url = "http://www.uspsoig.gov/document-library?"
+
+  since = options.get('since', None)
+  if since:
+    url += "&field_doc_date_value[value][date]=%s" % since
+
+  only = options.get('only', None)
+  if only:
+    only = only.split(",")
+    params = ["field_doc_cat_tid[]=%s" % CATEGORIES[id] for id in only]
+    url += "&%s" % str.join("&", params)
+
+  print url
+
   body = utils.download(url)
   doc = PyQuery(body)
+
+
   titles = doc(".views-row div h3")
   for title in titles:
     print title.text
 
+CATEGORIES = {
+  'audits': '1920',
+  'testimony': '1933',
+  'press': '1921',
+  'research': '1922',
+  'sarc': '3487',
+  'congress': '1923'
+}
 
 utils.run(run)
