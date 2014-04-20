59a33228d059de9f2500bd3b7223425585aa4085
Andrew Dai <theman@andrewdai.co>
2014-04-19 22:59:54 -0400

    will automatically downlaod all years data and got rid of unneed comments

--- inspectors/opm.py
+++ inspectors/opm.py
@@ -6,51 +6,23 @@ import bs4
 from datetime import datetime
 import calendar
 
-# This script's "run" function will be run.
-# You can do anything you want that eventually results in calling:
-#
-#   inspector.save_report(report)
-#
-# Where report is a dict with the following required fields:
-#
-#   inspector: the handle you chose for the IG, e.g. "usps"
-#   agency: the agency the report relates to.
-#           This can be the same value as the inspector field, but it
-#           may differ -- some IGs monitor multiple agencies.
-#   report_id: a unique ID for the report
-#   title: title of report
-#   url: link to report
-#   type: "report" (or some other class of document, if it makes sense to distinguish)
-#   published_on: date of publication
-#   year: year of publication
-#   file_type: 'pdf' or other file extension
-#
-# Any additional fields are fine, and will be kept.
-
 def run(options):
 
-  # Suggested flow, for an IG which paginates results.
-  print "## Downloading audits listing"
+  print "## Downloading audits and other reports"
   url = url_for()
   body = utils.download(url)
 
   doc = BeautifulSoup(body)
   results = doc.select("section")
-  print "## Downloading year %i " % int(results[0].get("title"))
-  listings = results[0].div.table.tbody.contents
-  for item in listings:
-    if type(item) is not bs4.element.Tag:
-      continue
-    report_from(item)
 
-  # for result in results:
-  #   try:
-  #     print "## Downloading year %i " % int(result.get("title"))
-  #   except ValueError:
-  #     continue
-  #   listings = result.div.table.tbody.contents
-  #   for item in listings:
-  #     report_from(item)
+  for result in results:
+    try:
+      print "## Downloading year %i " % int(result.get("title"))
+    except ValueError:
+      continue
+    listings = result.div.table.tbody.contents
+    for item in listings:
+      report_from(item)
 
 def url_for():
   return "http://www.opm.gov/our-inspector-general/reports/"
