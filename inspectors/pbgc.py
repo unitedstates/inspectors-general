3e25fb594f61f6951166623a14f504e14f9867a1
Steve Pulec <spulec@gmail.com>
2014-08-12 07:18:35 -0400

    Add congressional testimony.

--- inspectors/pbgc.py
+++ inspectors/pbgc.py
@@ -21,6 +21,7 @@ from utils import utils, inspector
 AUDIT_REPORTS_URL = "http://oig.pbgc.gov/evaluations/{year}.html"
 CONGRESSIONAL_REQUESTS_URL = "http://oig.pbgc.gov/requests.html"
 SEMIANNUAL_REPORTS_URL = "http://oig.pbgc.gov/reports.html"
+CONGRESSIONAL_TESTIMONY_URL = "http://oig.pbgc.gov/testimony.html"
 
 BASE_REPORT_URL = "http://oig.pbgc.gov/"
 
@@ -35,30 +36,38 @@ def run(options):
   year_range = inspector.year_range(options)
 
   # Pull the audit reports
-  # for year in year_range:
-  #   if year < 1998:  # The earliest year for audit reports
-  #     continue
-    # year_url = AUDIT_REPORTS_URL.format(year=year)
-    # doc = BeautifulSoup(utils.download(year_url))
-    # results = doc.select("tr")
-    # for result in results:
-    #   report = report_from(result, year_range)
-    #   if report:
-    #     inspector.save_report(report)
+  for year in year_range:
+    if year < 1998:  # The earliest year for audit reports
+      continue
+    year_url = AUDIT_REPORTS_URL.format(year=year)
+    doc = BeautifulSoup(utils.download(year_url))
+    results = doc.select("tr")
+    for result in results:
+      report = report_from(result, year_range)
+      if report:
+        inspector.save_report(report)
 
   # Pull the congreesional requests
-  # doc = BeautifulSoup(utils.download(CONGRESSIONAL_REQUESTS_URL))
-  # results = doc.select("tr")
-  # for result in results:
-  #   report = report_from(result, year_range)
-  #   if report:
-  #     inspector.save_report(report)
+  doc = BeautifulSoup(utils.download(CONGRESSIONAL_REQUESTS_URL))
+  results = doc.select("tr")
+  for result in results:
+    report = report_from(result, year_range)
+    if report:
+      inspector.save_report(report)
 
   # Pull the semiannual reports
   doc = BeautifulSoup(utils.download(SEMIANNUAL_REPORTS_URL))
   results =  doc.select("div.holder a")
   for result in results:
     report = semiannual_report_from(result, year_range)
+    if report:
+      inspector.save_report(report)
+
+  # Pull the congressional testimony
+  doc = BeautifulSoup(utils.download(CONGRESSIONAL_TESTIMONY_URL))
+  results =  doc.select("div.holder a")
+  for result in results:
+    report = testimony_report_from(result, year_range)
     if report:
       inspector.save_report(report)
 
@@ -123,6 +132,10 @@ def semiannual_report_from(result, year_range):
 
   title = " ".join(landing_page.select("h3")[0].text.split())
   relative_report_url = landing_page.find("a", text="Read Full Report").get('href')
+
+  # The relative report urls try to go up a level too many. Most browsers seem
+  # to just ignore this so we will too.
+  relative_report_url = relative_report_url.replace("../", "", 1)
   report_url = urljoin(SEMIANNUAL_REPORTS_URL, relative_report_url)
 
   # There is probably a way to be a bit smarter about this
@@ -149,6 +162,34 @@ def semiannual_report_from(result, year_range):
     report['summary'] = summary
   if landing_url:
     report['landing_url'] = landing_url
+  return report
+
+def testimony_report_from(result, year_range):
+  title = result.text
+  report_url = urljoin(BASE_REPORT_URL, result.get('href'))
+  report_filename = report_url.split("/")[-1]
+  report_id, _ = os.path.splitext(report_filename)
+
+  published_on_text = "-".join(re.search('\((\w+) (\d+), (\d{4})\)', result.text).groups())
+  try:
+    published_on = datetime.datetime.strptime(published_on_text, '%b-%d-%Y')
+  except ValueError:
+    published_on = datetime.datetime.strptime(published_on_text, '%B-%d-%Y')
+
+  if published_on.year not in year_range:
+    logging.debug("[%s] Skipping, not in requested range." % title)
+    return
+
+  report = {
+    'inspector': "pbgc",
+    'inspector_url': "http://oig.pbgc.gov",
+    'agency': "pbgc",
+    'agency_name': "Pension Benefit Guaranty Corporation",
+    'report_id': report_id,
+    'url': report_url,
+    'title': title,
+    'published_on': datetime.datetime.strftime(published_on, "%Y-%m-%d"),
+  }
   return report
 
 utils.run(run) if (__name__ == "__main__") else None
