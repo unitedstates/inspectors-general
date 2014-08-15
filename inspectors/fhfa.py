7c107f169103488f17a0087b4b7ad3ab99876218
Steve Pulec <spulec@gmail.com>
2014-08-14 20:31:40 -0400

    Add Federal Housing Financing Agency.

--- inspectors/fhfa.py
+++ inspectors/fhfa.py
@@ -3,7 +3,6 @@
 import datetime
 import logging
 import os
-from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 from utils import utils, inspector
@@ -20,6 +19,9 @@ from utils import utils, inspector
 #
 
 AUDIT_REPORTS_URL = "http://fhfaoig.gov/Reports/AuditsAndEvaluations?page={page}"
+MANAGEMENT_ALERTS_URL = "http://fhfaoig.gov/Reports/AdditionalActionItems"
+CONGRESSIONAL_TESTIMONY_URL = "http://fhfaoig.gov/testimony"
+SEMIANNUAL_REPORTS_URL = "http://fhfaoig.gov/Reports/Semiannual"
 
 # This will actually get adjusted downwards on the fly, so pick a huge number.
 # There are 4 pages of audits as of 2014-08-14, so let's try, er, 100.
@@ -43,15 +45,31 @@ def run(options):
       if report:
         inspector.save_report(report)
 
+  # Grab the other reports
+  for url in [MANAGEMENT_ALERTS_URL, CONGRESSIONAL_TESTIMONY_URL, SEMIANNUAL_REPORTS_URL]:
+    doc = BeautifulSoup(utils.download(url))
+    results = doc.select(".views-field")
+    if not results:
+      results = doc.select(".views-row")
+    for result in results:
+      report = report_from(result, year_range)
+      if report:
+        inspector.save_report(report)
+
 def report_from(result, year_range):
   link = result.find("a")
 
   report_url = link.get('href')
-  report_id = link.next_sibling.replace("(", "").replace(")", "").strip().rstrip("-").strip()
+  report_filename = report_url.split("/")[-1]
+  report_id, _ = os.path.splitext(report_filename)
   title = link.text
 
-  published_on_text = result.select("span.date-display-single")[0].text
-  published_on = datetime.datetime.strptime(published_on_text, '%m/%d/%Y')
+  try:
+    published_on_text = result.select("span.date-display-single")[0].text
+    published_on = datetime.datetime.strptime(published_on_text, '%m/%d/%Y')
+  except IndexError:
+    published_on_text = result.select("span.date-display-end")[0].text
+    published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
 
   if published_on.year not in year_range:
     logging.debug("[%s] Skipping, not in requested range." % report_url)
