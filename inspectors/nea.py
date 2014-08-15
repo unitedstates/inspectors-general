5462b1dbd590647d3716b581b555d8f25c9ef450
Steve Pulec <spulec@gmail.com>
2014-08-14 22:44:56 -0400

    Add National Endowment for the Arts.

--- inspectors/nea.py
+++ inspectors/nea.py
@@ -18,12 +18,14 @@ from utils import utils, inspector
 #
 
 AUDIT_REPORTS_URL = "http://arts.gov/oig/reports/audits"
+SPECIAL_REVIEWS_URL = "http://arts.gov/oig/reports/specials"
+SEMIANNUAL_REPORTS_URL = "http://arts.gov/oig/reports/semi-annual"
 
 def run(options):
   year_range = inspector.year_range(options)
 
   # Pull the reports
-  for url in [AUDIT_REPORTS_URL]:
+  for url in [AUDIT_REPORTS_URL, SPECIAL_REVIEWS_URL, SEMIANNUAL_REPORTS_URL]:
     doc = BeautifulSoup(utils.download(url))
     results = doc.select("div.field-item li")
     for result in results:
@@ -33,15 +35,21 @@ def run(options):
 
 def report_from(result, landing_url, year_range):
   link = result.find("a")
+  if not link:
+    return
 
   title = link.text
   report_url = urljoin(landing_url, link.get('href'))
   report_filename = report_url.split("/")[-1]
   report_id, _ = os.path.splitext(report_filename)
 
-  # For reports where we can only find the year, set them to Nov 1st of that year
-  published_on_year = int(result.find_previous("h3").text.strip())
-  published_on = datetime.datetime(published_on_year, 11, 1)
+  try:
+    published_on_text = title.split("-")[-1].split("–")[-1].strip()
+    published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
+  except ValueError:
+    # For reports where we can only find the year, set them to Nov 1st of that year
+    published_on_year = int(result.find_previous("h3").text.strip())
+    published_on = datetime.datetime(published_on_year, 11, 1)
 
   if published_on.year not in year_range:
     logging.debug("[%s] Skipping, not in requested range." % report_url)
