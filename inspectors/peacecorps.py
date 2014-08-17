3a76bc7056f6dbd8410862607814826898cdf622
Steve Pulec <spulec@gmail.com>
2014-08-17 19:36:26 -0400

    Add additional reports.

--- inspectors/peacecorps.py
+++ inspectors/peacecorps.py
@@ -18,11 +18,19 @@ from utils import utils, inspector
 
 REPORTS_URL = "http://www.peacecorps.gov/about/inspgen/reports/"
 
-SECTION_TITLES_TO_SKIP = [
-  "Management Advisory Reports",
-  "Peer Reviews",
-  "Special Reports/Reviews",
-]
+REPORT_PUBLISHED_MAPPING = {
+  "Burkina_Faso_Medical_Supply_Management_Advisory_Report": datetime.datetime(2013, 3, 14),
+  "PCIG_Final_MAR_Certification_of_Volunteer_Payments": datetime.datetime(2013, 9, 24),
+  "MAR_Cost_Savings_Opportunity_on_Value_Added_Tax": datetime.datetime(2013, 2, 13),
+  "Management_Advisory_Report-Peace_Corps_Drug_Free_Workplace_Program": datetime.datetime(2012, 8, 16),
+  "PCIG_2014_Peace_Corps_OIG_Peer_Review_Final": datetime.datetime(2014, 3, 27),
+  "MAR_Sierra_Leone": datetime.datetime(2013, 3, 14),
+  "PC_Morocco_Assessment_of_Medical_Care": datetime.datetime(2010, 2, 1),
+  "PC_Recurring_Issues_OIG_Post_Audits_Evaluations_FYs_2009-2011": datetime.datetime(2012, 4, 1),
+  "PC_Vanuatu_SR_Advice_and_Assistance": datetime.datetime(2010, 5, 1),
+  "PC_Gambia_SR_Grant_Activities": datetime.datetime(2010, 5, 14),
+  "PC_Ecuador_Special_Review_IG1005SR": datetime.datetime(2010, 9, 1),
+}
 
 def run(options):
   year_range = inspector.year_range(options)
@@ -43,18 +51,19 @@ def report_from(result, year_range):
   title = link.text
 
   section_title = result.find_previous("h3").text.strip()
-  if section_title in SECTION_TITLES_TO_SKIP:
-    return
 
   estimated_date = False
-  try:
-    published_on_text = title.split("–")[-1].strip()
-    published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
-  except ValueError:
-    # For reports where we can only find the year, set them to Nov 1st of that year
-    published_on_year =int(section_title.lstrip("FY "))
-    published_on = datetime.datetime(published_on_year, 11, 1)
-    estimated_date = True
+  if report_id in REPORT_PUBLISHED_MAPPING:
+    published_on = REPORT_PUBLISHED_MAPPING[report_id]
+  else:
+    try:
+      published_on_text = title.split("–")[-1].strip()
+      published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
+    except ValueError:
+      # For reports where we can only find the year, set them to Nov 1st of that year
+      published_on_year =int(section_title.lstrip("FY "))
+      published_on = datetime.datetime(published_on_year, 11, 1)
+      estimated_date = True
 
   if published_on.year not in year_range:
     logging.debug("[%s] Skipping, not in requested range." % report_url)
