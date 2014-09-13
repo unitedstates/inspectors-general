109fd91a27d1bdc8b949c64dc38bc217fe6e1dff
David Cook <divergentdave@gmail.com>
2014-09-13 12:13:15 -0500

    [hhs] Fix indentation from last commit
    Split this out to keep the diffs readable

--- inspectors/hhs.py
+++ inspectors/hhs.py
@@ -455,45 +455,45 @@ def published_on_from_inline_link(result, report_filename, title, report_id, rep
       except (AttributeError, ValueError):
         pass
   if published_on == None:
+    try:
+      fiscal_year = int(result.text.split(":")[0].split()[1])
+      published_on = datetime.datetime(fiscal_year - 1, 10, 1)
+    except (ValueError, IndexError):
       try:
-        fiscal_year = int(result.text.split(":")[0].split()[1])
+        fiscal_year = int(report_filename.split("-")[0])
         published_on = datetime.datetime(fiscal_year - 1, 10, 1)
-      except (ValueError, IndexError):
+      except ValueError:
         try:
-          fiscal_year = int(report_filename.split("-")[0])
-          published_on = datetime.datetime(fiscal_year - 1, 10, 1)
+          published_on = datetime.datetime.strptime(title.replace(": ", ":"), "Compendium:%B %Y Edition")
         except ValueError:
           try:
-            published_on = datetime.datetime.strptime(title.replace(": ", ":"), "Compendium:%B %Y Edition")
+            published_on = datetime.datetime.strptime(report_id.split("-")[-1], "%m%d%Y")
           except ValueError:
             try:
-              published_on = datetime.datetime.strptime(report_id.split("-")[-1], "%m%d%Y")
-            except ValueError:
+              report_year = int(report_url.split("/")[-2:-1][0])
+              published_on = datetime.datetime(report_year, 1, 1)
+            except (ValueError, IndexError):
               try:
-                report_year = int(report_url.split("/")[-2:-1][0])
-                published_on = datetime.datetime(report_year, 1, 1)
-              except (ValueError, IndexError):
-                try:
-                  fiscal_year = int(title.replace("Fiscal Year ", ""))
-                  published_on = datetime.datetime(fiscal_year - 1, 10, 1)
-                except ValueError:
-                  # Try using the last-modified header
-                  response = utils.scraper.request(method='HEAD', url=report_url)
-                  last_modified = response.headers['Last-Modified']
-                  published_on = datetime.datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
-                  if published_on.year < 2003:
-                    # We don't trust the last-modified for dates before 2003
-                    # since a lot of historical reports were published at this
-                    # time. For these reports, fallback to a hacky method based
-                    # on the report id. For example: oei-04-12-00490. These are
-                    # the dates that the report_id was assigned which is before
-                    # the report was actually published
-                    published_on_text = "-".join(report_id.split("-")[1:3])
-                    try:
-                      published_on = datetime.datetime.strptime(published_on_text, '%m-%y')
-                    except ValueError:
-                      pass
-                      # Fall back to the Last-Modified header
+                fiscal_year = int(title.replace("Fiscal Year ", ""))
+                published_on = datetime.datetime(fiscal_year - 1, 10, 1)
+              except ValueError:
+                # Try using the last-modified header
+                response = utils.scraper.request(method='HEAD', url=report_url)
+                last_modified = response.headers['Last-Modified']
+                published_on = datetime.datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
+                if published_on.year < 2003:
+                  # We don't trust the last-modified for dates before 2003
+                  # since a lot of historical reports were published at this
+                  # time. For these reports, fallback to a hacky method based
+                  # on the report id. For example: oei-04-12-00490. These are
+                  # the dates that the report_id was assigned which is before
+                  # the report was actually published
+                  published_on_text = "-".join(report_id.split("-")[1:3])
+                  try:
+                    published_on = datetime.datetime.strptime(published_on_text, '%m-%y')
+                  except ValueError:
+                    pass
+                    # Fall back to the Last-Modified header
   return published_on
 
 def get_subtopic_map(topic_url):
