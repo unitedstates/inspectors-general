023252d76685f27f2c0d7d681564c2a853a618c5
David Cook <divergentdave@gmail.com>
2014-09-17 20:17:06 -0500

    404s: false negatives and utf-8
    Check websites with bad 404s to make sure the regular expressions still
    work.
    Try decoding files as utf-8 first, then fall back to bytes.

--- scripts/soft_404.py
+++ scripts/soft_404.py
@@ -3,23 +3,35 @@
 import os, os.path
 import re
 
-PAGE_NOT_FOUND_RE = re.compile(b"(<title>(404 Page Not Found - CFTC|CPB: Page Not Found|DoD IG - Error Message|404: NOT FOUND|Page Not Found|Maintenance|Page Not Found Smithsonian|404)</title>|That page was not found\\.&#160; If possible we will redirect you to that content now\\.|The Office of Inspector General [(]OIG[)] is an independent unit established by law which is responsible for promoting economy, efficiency, and effectiveness and detecting and preventing fraud, waste, and mismanagement in the General Services Administration's [(]GSA[)] programs and operations\\.)")
+PAGE_NOT_FOUND_PATTERN = b"(<title>(404 Page Not Found - CFTC|CPB: Page Not Found|DoD IG - Error Message|404: NOT FOUND|Page Not Found|Maintenance|Page Not Found Smithsonian|404)</title>|That page was not found\\.&#160; If possible we will redirect you to that content now\\.|The Office of Inspector General [(]OIG[)] is an independent unit established by law which is responsible for promoting economy, efficiency, and effectiveness and detecting and preventing fraud, waste, and mismanagement in the General Services Administration's [(]GSA[)] programs and operations\\.)"
+PAGE_NOT_FOUND_BYTES_RE = re.compile(PAGE_NOT_FOUND_PATTERN)
+PAGE_NOT_FOUND_STRING_RE = re.compile(PAGE_NOT_FOUND_PATTERN.decode('ascii'))
 
-IGS_WITH_BAD_404 = (
-  'cftc',
-  'cpb',
-  'dod',
-  'exim',
-  'fec',
-  'gpo',
-  'gsa',
-  'ncua',
-  'smithsonian',
-  'state'
-)
+URLS = {
+  'cftc': 'http://www.cftc.gov/About/OfficeoftheInspectorGeneral/doesyour404work',
+  'cpb': 'http://www.cpb.org/oig/doesyour404work',
+  'dod': 'http://www.dodig.mil/doesyour404work',
+  'exim': 'http://www.exim.gov/oig/doesyour404work',
+  'fec': 'http://www.fec.gov/fecig/doesyour404work',
+  'gpo': 'http://www.gpo.gov/oig/doesyour404work',
+  'gsa': 'http://gsaig.gov/doesyour404work',
+  'ncua': 'http://www.ncua.gov/about/Leadership/Pages/doesyour404work',
+  'smithsonian': 'http://www.si.edu/OIG/doesyour404work',
+  'state': 'http://oig.state.gov/doesyour404work'
+}
+
+IGS_WITH_BAD_404 = tuple(URLS.keys())
 
 def run(ig_list):
   from inspectors.utils import utils
+
+  for inspector, url in URLS.items():
+    if not ig_list or inspector in ig_list:
+      result = utils.scraper.urlopen(url)
+      match = PAGE_NOT_FOUND_STRING_RE.search(result)
+      if not match:
+        print("False negative for %s" % inspector)
+
   data_dir = utils.data_dir()
   for inspector in os.listdir(data_dir):
     if (not ig_list or inspector in ig_list) and inspector in IGS_WITH_BAD_404:
@@ -28,7 +40,13 @@ def run(ig_list):
         for dirpath, dirnames, filenames in os.walk(inspector_path):
           for filename in filenames:
             path = os.path.join(dirpath, filename)
-            f = open(path, 'rb')
-            for line in f:
-              if PAGE_NOT_FOUND_RE.search(line):
-                print("Soft 404 found: %s" % path)
+            try:
+              f = open(path, 'r', encoding='utf-8')
+              for line in f:
+                if PAGE_NOT_FOUND_STRING_RE.search(line):
+                  print("Soft 404 found: %s" % path)
+            except UnicodeDecodeError:
+              f = open(path, 'rb')
+              for line in f:
+                if PAGE_NOT_FOUND_BYTES_RE.search(line):
+                  print("Soft 404 found: %s" % path)
