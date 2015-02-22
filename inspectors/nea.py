fed31550cdc5af28311e1da728456d97dd9c5612
Steve Pulec <spulec@gmail.com>
2015-02-22 15:41:16 -0500

    Update NEA to get FISMA reports back. Closes #186.

--- inspectors/nea.py
+++ inspectors/nea.py
@@ -17,23 +17,25 @@ archive = 2005
 #   report_id: only bother to process a single report
 #
 # Notes for IG's web team:
-#
+# - Fix MISSING_IDS
 
 AUDIT_REPORTS_URL = "http://arts.gov/oig/reports/audits"
 SPECIAL_REVIEWS_URL = "http://arts.gov/oig/reports/specials"
 SEMIANNUAL_REPORTS_URL = "http://arts.gov/oig/reports/semi-annual"
 PEER_REVIEWS_URL = "http://arts.gov/oig/reports/external-peer-reviews"
-
-# NEA took this section down sometime in
-# FISMA_REPORTS_URL = "http://arts.gov/oig/reports/fisma"
+FISMA_REPORTS_URL = "http://arts.gov/inspector-general/reports/internal-reviews"
 
 REPORT_URLS = {
   "audit": AUDIT_REPORTS_URL,
   "evaluation": SPECIAL_REVIEWS_URL,
   "semiannual_report": SEMIANNUAL_REPORTS_URL,
   "peer_review": PEER_REVIEWS_URL,
-  # "fisma": FISMA_REPORTS_URL,
+  "fisma": FISMA_REPORTS_URL,
 }
+
+MISSING_IDS = [
+  "EA-perimeter-security-test-reload",
+]
 
 def run(options):
   year_range = inspector.year_range(options, archive)
@@ -44,6 +46,10 @@ def run(options):
   for report_type, url in REPORT_URLS.items():
     doc = BeautifulSoup(utils.download(url))
     results = doc.select("div.field-item li")
+    if not results:
+      results = doc.select("div.field-item tr")
+    if not results:
+      raise AssertionError("No report links found for %s" % url)
     for result in results:
       report = report_from(result, url, report_type, year_range)
 
@@ -66,16 +72,20 @@ def report_from(result, landing_url, report_type, year_range):
 
   estimated_date = False
   try:
-    published_on_text = title.split("-")[-1].split("–")[-1].strip()
-    published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
-  except ValueError:
-    # For reports where we can only find the year, set them to Nov 1st of that year
+    published_on_text = result.select("td")[1].text.strip()
+    published_on = datetime.datetime.strptime(published_on_text, '%m/%d/%y')
+  except (ValueError, IndexError):
     try:
-      published_on_year = int(result.find_previous("h3").text.strip())
-    except AttributeError:
-      published_on_year = int(re.search('(\d+)', title).group())
-    published_on = datetime.datetime(published_on_year, 11, 1)
-    estimated_date = True
+      published_on_text = title.split("-")[-1].split("–")[-1].strip()
+      published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
+    except ValueError:
+      # For reports where we can only find the year, set them to Nov 1st of that year
+      try:
+        published_on_year = int(result.find_previous("strong").text.strip())
+      except AttributeError:
+        published_on_year = int(re.search('(\d+)', title).group())
+      published_on = datetime.datetime(published_on_year, 11, 1)
+      estimated_date = True
 
   if published_on.year not in year_range:
     logging.debug("[%s] Skipping, not in requested range." % report_url)
@@ -87,6 +97,7 @@ def report_from(result, landing_url, report_type, year_range):
     'agency': 'nea',
     'agency_name': 'National Endowment for the Arts',
     'type': report_type,
+    'landing_url': landing_url,
     'report_id': report_id,
     'url': report_url,
     'title': title,
@@ -94,6 +105,10 @@ def report_from(result, landing_url, report_type, year_range):
   }
   if estimated_date:
     report['estimated_date'] = estimated_date
+  if report_id in MISSING_IDS:
+    report['unreleased'] = True
+    report['missing'] = True
+    report['url'] = None
   return report
 
 utils.run(run) if (__name__ == "__main__") else None
