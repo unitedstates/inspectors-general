53542fdce4b415fdb30360cb4a6121c17bffbfd1
David Cook <divergentdave@gmail.com>
2015-03-01 11:35:20 -0600

    [agriculture] Handle more typos

--- inspectors/agriculture.py
+++ inspectors/agriculture.py
@@ -213,8 +213,8 @@ def report_from(result, page_url, year_range, report_type, agency_slug="agricult
     title = title.title()
 
   # These entries on the IG page have the wrong URLs associated with them. The
-  # correct URLs were retrieved from an earlier version of the page, via the
-  # Internet Archive Wayback Machine.
+  # correct URLs were guessed or retrieved from an earlier version of the page,
+  # via the Internet Archive Wayback Machine.
   if report_url == "http://www.usda.gov/oig/webdocs/IGtestimony110302.pdf" and \
       title == "Statement Of Phyllis K. Fong Inspector General: Before The " \
       "House Appropriations Subcommittee On Agriculture, Rural Development, " \
@@ -231,6 +231,13 @@ def report_from(result, page_url, year_range, report_type, agency_slug="agricult
       "Senate Committee On Agriculture, Nutrition, And Forestry On The " \
       "Department's Processing Of Civil Rights Complaints":
     report_url = "http://www.usda.gov/oig/webdocs/IGstestimony.PDF"
+  elif report_url == "http://www.usda.gov/oig/webdocs/34601-10-TE.pdf" and \
+      title == "Rural Housing Service Single Family Housing Program - Maine":
+    report_url = "http://www.usda.gov/oig/webdocs/04004-05-Hy.pdf"
+  elif report_url == "http://www.usda.gov/oig/webdocs/04004-05-Hy.pdf" and \
+      title == "Rural Development\u2019s Processing of Loan Guarantees to " \
+      "Member of the Western Sugar Cooperative":
+    report_url = "http://www.usda.gov/oig/webdocs/34601-03-Ch.pdf"
 
   # This report is listed twice on the same page with slightly different titles
   if title == "Animal and Plant Health Inspection Service Transition and " \
@@ -239,6 +246,16 @@ def report_from(result, page_url, year_range, report_type, agency_slug="agricult
 
   report_filename = report_url.split("/")[-1]
   report_id = os.path.splitext(report_filename)[0]
+
+  # Differentiate between two letters on the same report
+  if report_url == "http://www.usda.gov/oig/webdocs/34099-12-TE.pdf":
+    report_id = "34099-12-Te_1"
+  elif report_url == "http://www.usda.gov/oig/webdocs/34099-12-Te.pdf":
+    report_id = "34099-12-Te_2"
+
+  if title == "American Recovery and Reinvestment Act - Emergency Watershed " \
+      "Protection Program Floodplain Easements" and report_id == "10703-1-KC":
+    return
 
   # These are just summary versions of other reports. Skip for now.
   if '508 Compliant Version' in title:
