3fb8dd73f386d15bd61a4165b1c65201b0d07b04
David Cook <divergentdave@gmail.com>
2015-07-26 19:53:07 -0500

    [lsc] Rewrite scraper for new website

--- inspectors/lsc.py
+++ inspectors/lsc.py
@@ -6,10 +6,10 @@ import os
 import re
 from urllib.parse import urljoin, unquote
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString, Comment
 from utils import utils, inspector
 
-# http://www.oig.lsc.gov
+# https://www.oig.lsc.gov
 archive = 1994
 
 # options:
@@ -18,14 +18,35 @@ archive = 1994
 # Notes for IG's web team:
 #
 
-AUDIT_REPORTS_URL = "http://www.oig.lsc.gov/rpts/audit.htm"
-FINANCIAL_STATEMENTS_URL = "http://www.oig.lsc.gov/rpts/corp.htm"
-OTHER_REPORTS_URL = "http://www.oig.lsc.gov/rpts/other.htm"
-SEMIANNUAL_REPORTS_URL = "http://www.oig.lsc.gov/sar/sar.htm"
-CLIENT_TRUST_FUND_URL = "http://www.oig.lsc.gov/rpts/ctf.htm"
+SEMIANNUAL_REPORTS_URL = "https://www.oig.lsc.gov/products/sar"
+AUDIT_REPORTS_URL = "https://www.oig.lsc.gov/products/audit-reports"
+INVESTIGATIONS_URL = "https://www.oig.lsc.gov/products/investigative-results-and-products"
+PEER_REVIEWS_URL = "https://www.oig.lsc.gov/products/peer-reviews"
+SEMIANNUAL_REPORTS_ARCHIVE_URL = "https://www.oig.lsc.gov/products/report-archives/sar-archives"
+AUDIT_REPORTS_ARCHIVE_URL = "https://www.oig.lsc.gov/products/report-archives/audit-reports"
+OTHER_REPORTS_ARCHIVE_URL = "https://www.oig.lsc.gov/products/report-archives/other-reports"
+MAPPING_PROJECT_ARCHIVE_URL = "https://www.oig.lsc.gov/products/report-archives/mapping-project"
+MAPPING_PROJECT_ARCHIVE_GRANTEE_URL = "https://www.oig.lsc.gov/grantee-evaluation"
 
 REPORT_PUBLISHED_MAP = {
-  '14-06': datetime.datetime(2014, 6, 30)
+  "14-06": datetime.datetime(2014, 6, 30),
+  "fraud-alert-15-02": datetime.datetime(2015, 4, 22),
+  "check_fraud_attachment": datetime.datetime(2012, 1, 18),
+  "lscoighotline": datetime.datetime(2008, 12, 17),
+  "Dec_2006_Fraud_Alert": datetime.datetime(2006, 12, 11),
+  "2014-report-to-nations": datetime.datetime(2014, 1, 1),
+  "TIS_SASS_1014_LSC_OIG": datetime.datetime(2014, 10, 1),
+  "MeekerOIGMappingReport": datetime.datetime(2005, 9, 14),
+  "core-legal-services": datetime.datetime(2005, 3, 14),
+  "Mapping_Evaluation_Phase_I_Volume_I_Final_Report": datetime.datetime(2003, 11, 1),
+  "evalicls": datetime.datetime(2005, 11, 1),
+  "evallafla": datetime.datetime(2004, 11, 1),
+  "evallasoc": datetime.datetime(2004, 11, 1),
+  "evallassd": datetime.datetime(2004, 11, 1),
+  "evalnls": datetime.datetime(2004, 11, 1),
+  "evalalas": datetime.datetime(2005, 11, 1),
+  "evalglsp": datetime.datetime(2005, 11, 1),
+  "evalmlsa": datetime.datetime(2005, 11, 1),
 }
 
 BLACKLIST_REPORT_TITLES = [
@@ -37,68 +58,173 @@ BLACKLIST_REPORT_TITLES = [
   'Client Trust Fund Inspection Reports',
 ]
 
-REPORT_URLS = {
-  SEMIANNUAL_REPORTS_URL: "semiannual_report",
-  OTHER_REPORTS_URL: "other",
-  FINANCIAL_STATEMENTS_URL: "audit",
-  AUDIT_REPORTS_URL: "audit",
-  CLIENT_TRUST_FUND_URL: "audit",
-}
+def parse_year_accordion(content, landing_url, report_type, year_range):
+  accordions = content.select("div.accordion-group")
+  if not accordions:
+    raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" %
+                                        landing_url)
+  for accordion in accordions:
+    heading = accordion.select("div.accordion-heading")[0]
+    year_text = inspector.sanitize(heading.text)
+    if year_text.startswith("FY"):
+      year = int(year_text[2:])
+    else:
+      year = int(year_text)
+    body = accordion.select("div.accordion-body div.accordion-inner")[0]
+    if year_text == "FY1995" and body.text.strip() == "FY1995":
+      continue
+    results = [a for a in body.find_all("a") if a.text.strip()]
+    if not results:
+      raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" %
+                                          landing_url)
+    for result in results:
+      report = report_from(result, landing_url, report_type, year_range, year)
+      if report:
+        inspector.save_report(report)
+
+def parse_investigation(content, landing_url, report_type, year_range):
+  doj_flag = True
+  doj_report_counter = 0
+  other_report_counter = 0
+  for child in content.children:
+    if isinstance(child, Tag) and child.name == 'hr':
+      doj_flag = False
+      continue
+    if doj_flag:
+      if isinstance(child, Tag) and child.name == 'ul':
+        report = report_from(child.li, landing_url, report_type, year_range)
+        if report:
+          inspector.save_report(report)
+          doj_report_counter = doj_report_counter + 1
+    else:
+      if isinstance(child, Tag):
+        if child.name != 'h3' and child.text.strip():
+          report = report_from(child, landing_url, report_type, year_range)
+          if report:
+            inspector.save_report(report)
+            other_report_counter = other_report_counter + 1
+      elif isinstance(child, Comment):
+        continue
+      elif isinstance(child, NavigableString):
+        if child.strip():
+          raise Exception("Unexpected text!: " + child)
+  if doj_report_counter == 0 or other_report_counter == 0:
+    raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" % landing_url)
+
+def parse_peer_reviews(content, landing_url, report_type, year_range):
+  links = content.find_all("a")
+  if len(links) <= 1:
+    raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" % landing_url)
+  for link in links:
+    if link.text.find("Government Auditing Standards") != -1:
+      continue
+    result = link.parent
+    report = report_from(result, landing_url, report_type, year_range)
+    if report:
+      inspector.save_report(report)
+
+def parse_mapping(content, landing_url, report_type, year_range):
+  links = content.find_all("a")
+  if not links:
+    raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" % landing_url)
+  for link in links:
+    href = link.get("href")
+    href = urljoin(landing_url, href)
+    result = None
+    if href == "https://www.oig.lsc.gov/images/mapping/mapping.zip":
+      continue
+    elif href == MAPPING_PROJECT_ARCHIVE_GRANTEE_URL:
+      continue
+    elif href.startswith("mailto:"):
+      continue
+    elif href == "https://www.oig.lsc.gov/evaluation-of-legal-services-mapping-prsentation":
+      link["href"] = "https://oig.lsc.gov/mapping/phaseIIbriefing.pdf"
+      result = link.parent
+    elif href in ("https://www.oig.lsc.gov/images/pdfs/mapping/MeekerOIGMappingReport.pdf",
+                  "https://www.oig.lsc.gov/core-legal-services",):
+      result = link.parent
+    elif href == "https://www.oig.lsc.gov/images/mapping/Mapping_Evaluation_Phase_I_Volume_I_Final_Report.pdf":
+      result = link.parent.parent
+    elif (href.startswith("https://oig.lsc.gov/mapping/references/eval") and
+          href.endswith(".pdf")):
+      result = link
+    else:
+      raise Exception("Unexpected link found on a mapping project page: %s"
+                      % href)
+
+    report = report_from(result, landing_url, report_type, year_range)
+    if report:
+      inspector.save_report(report)
+
+REPORT_PAGES_INFO = [
+  (SEMIANNUAL_REPORTS_URL, "semiannual_report", parse_year_accordion),
+  (AUDIT_REPORTS_URL, "audit", parse_year_accordion),
+  (INVESTIGATIONS_URL, "investigation", parse_investigation),
+  (PEER_REVIEWS_URL, "other", parse_peer_reviews),
+  (SEMIANNUAL_REPORTS_ARCHIVE_URL, "semiannual_report", parse_year_accordion),
+  (AUDIT_REPORTS_ARCHIVE_URL, "audit", parse_year_accordion),
+  (OTHER_REPORTS_ARCHIVE_URL, "other", parse_year_accordion),
+  (MAPPING_PROJECT_ARCHIVE_URL, "other", parse_mapping),
+  (MAPPING_PROJECT_ARCHIVE_GRANTEE_URL, "other", parse_mapping),
+]
 
 def run(options):
   year_range = inspector.year_range(options, archive)
 
   # Pull the audit reports
-  for url, report_type in REPORT_URLS.items():
+  for url, report_type, parse_func in REPORT_PAGES_INFO:
     page_content = utils.download(url)
-
-    # This typo confuses BS4 and interferes with our selectors
-    page_content = page_content.replace('<h4>2015</h3>', '<h4>2015</h4>')
-
     doc = BeautifulSoup(page_content)
 
-    results = doc.select("blockquote > ul > a")
-    if not results:
-      results = doc.select("blockquote > ul > li > a")
-    if not results:
-      results = doc.select("blockquote > font > ul > a")
-    if not results:
-      results = doc.select("blockquote > a")
-    if not results:
-      raise inspector.NoReportsFoundError("Legal Services Corporation (%s)" % url)
-    for result in results:
-      report = report_from(result, url, report_type, year_range)
-      if report:
-        inspector.save_report(report)
+    content = doc.select("section.article-content")[0]
+    parse_func(content, url, report_type, year_range)
 
 REPORT_NO_RE = re.compile("[0-9]{2}-[0-9]{2,3}[A-Z]?")
 
-def report_from(result, landing_url, report_type, year_range):
+def report_from(result, landing_url, report_type, year_range, year=None):
   if not result.text or result.text in BLACKLIST_REPORT_TITLES:
     # There are a few empty links due to bad html and some links for alternative
     # formats (PDF) that we will just ignore.
     return
 
-  report_url = urljoin(landing_url, result.get('href'))
-  report_filename = report_url.split("/")[-1]
+  link_text = None
+  if result.name == 'a':
+    report_url = result.get('href')
+    link_text = inspector.sanitize(result.text)
+    title = inspector.sanitize("%s %s" % (result.text, result.next_sibling))
+  else:
+    links = [link for link in result.find_all('a') if link.text.strip()]
+    report_url = links[0].get('href')
+    link_text = inspector.sanitize(result.a.text)
+    title = inspector.sanitize(result.text)
+  report_url = urljoin(landing_url, report_url)
+  report_filename = os.path.basename(report_url)
 
-  report_no_match = REPORT_NO_RE.match(result.text.strip())
+  prev = result.previous_sibling
+  if isinstance(prev, NavigableString) and "See, also:" in prev:
+    return None
+
+  report_no_match = REPORT_NO_RE.match(link_text)
   if report_no_match:
     report_id = report_no_match.group(0)
-    if landing_url == CLIENT_TRUST_FUND_URL:
+    if "fraud" in report_url.lower():
+      report_id = "fraud-alert-" + report_id
+    elif "Client_Trust_Fund" in report_url:
       report_id = "CTF-" + report_id
+    elif report_filename.startswith("sr"):
+      report_id = "special-report-" + report_id
   else:
     report_id, _ = os.path.splitext(report_filename)
     report_id = unquote(report_id)
   report_id = "-".join(report_id.split())
   report_id = report_id.replace("\\", "") # strip backslashes
 
-  title = "{} {}".format(result.text.strip(), result.next_sibling)
-
   estimated_date = False
   published_on = None
   if report_id in REPORT_PUBLISHED_MAP:
     published_on = REPORT_PUBLISHED_MAP[report_id]
+  elif link_text == "June 2015":
+    published_on = datetime.datetime(2015, 6, 1)
   else:
     try:
       published_on_text = re.search('(\d+/\d+/\d+)', title).groups()[0]
@@ -109,15 +235,18 @@ def report_from(result, landing_url, report_type, year_range):
         try:
           published_on_text = re.search('(\d+/\d+)', title).groups()[0]
         except AttributeError:
+          if year is None:
+            raise Exception("No date or year was detected for %s (%s)" %
+                            (report_id, title))
           # Since we only have the year, set this to Nov 1st of that year
-          published_on_year = int(result.find_previous("h3").text)
-          published_on = datetime.datetime(published_on_year, 11, 1)
+          published_on = datetime.datetime(year, 11, 1)
           estimated_date = True
 
     if not published_on:
       datetime_formats = [
         '%B %d, %Y',
         '%m/%d/%Y',
+        '%m/%d/%y',
         '%m/%Y',
         '%m/%y'
       ]
@@ -135,7 +264,7 @@ def report_from(result, landing_url, report_type, year_range):
 
   report = {
     'inspector': 'lsc',
-    'inspector_url': 'http://www.oig.lsc.gov',
+    'inspector_url': 'https://www.oig.lsc.gov',
     'agency': 'lsc',
     'agency_name': 'Legal Services Corporation',
     'type': report_type,
@@ -144,8 +273,13 @@ def report_from(result, landing_url, report_type, year_range):
     'title': title,
     'published_on': datetime.datetime.strftime(published_on, "%Y-%m-%d"),
   }
+
   if estimated_date:
     report['estimated_date'] = estimated_date
+
+  if report_url in ("https://www.oig.lsc.gov/core-legal-services"):
+    report['file_type'] = "html"
+
   return report
 
 utils.run(run) if (__name__ == "__main__") else None
