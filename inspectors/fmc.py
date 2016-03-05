e871abc422fb94f9c7d8ad6a59a090ea4baf403e
David Cook <divergentdave@gmail.com>
2016-03-04 20:43:28 -0600

    [fmc] Remove year fallback, add dates

--- inspectors/fmc.py
+++ inspectors/fmc.py
@@ -3,7 +3,6 @@
 import datetime
 import logging
 import os
-import re
 from urllib.parse import urljoin
 
 from utils import utils, inspector
@@ -38,6 +37,70 @@ BLACKLIST_REPORT_URLS = [
   "http://www.fmc.gov/UserFiles/pages/File/OIG_Final_Report_A09-06.pdf",
   "http://www.fmc.gov/UserFiles/pages/File/OIG_Final_Report_A09-07.pdf",
 ]
+
+REPORT_PUBLISHED_MAP = {
+  "A16-01": datetime.datetime(2015, 11, 10),
+  "A16-02": datetime.datetime(2015, 11, 12),
+  "A15-01": datetime.datetime(2014, 11, 14),
+  "A15-01A": datetime.datetime(2014, 12, 31),
+  "A15-02": datetime.datetime(2014, 11, 14),
+  "A15-03": datetime.datetime(2014, 11, 14),
+  "A15-04": datetime.datetime(2015, 3, 10),
+  "A15-05": datetime.datetime(2015, 9, 25),
+  "A14-01": datetime.datetime(2013, 12, 12),
+  "A14-01A": datetime.datetime(2014, 1, 31),
+  "A14-02": datetime.datetime(2014, 1, 3),
+  "A13-01": datetime.datetime(2012, 11, 6),
+  "A13-02": datetime.datetime(2012, 12, 6),
+  "A13-03": datetime.datetime(2012, 12, 21),
+  "A13-04": datetime.datetime(2012, 12, 14),
+  "A13-04A": datetime.datetime(2013, 3, 15),
+  "A12-01": datetime.datetime(2011, 11, 9),
+  "A12-02": datetime.datetime(2012, 1, 17),
+  "OR12-01": datetime.datetime(2012, 3, 2),
+  "A12-01A": datetime.datetime(2012, 3, 5),
+  "OR12-02": datetime.datetime(2012, 7, 17),
+  "OR11-02": datetime.datetime(2011, 9, 30),
+  "OR11-01": datetime.datetime(2011, 3, 16),
+  "A11-02A": datetime.datetime(2011, 1, 31),
+  "A11-02": datetime.datetime(2010, 11, 10),
+  "A11-01A": datetime.datetime(2010, 11, 8),
+  "A11-01": datetime.datetime(2010, 12, 15),
+  "OR10-04": datetime.datetime(2010, 8, 6),
+  "OR10-03": datetime.datetime(2010, 5, 27),
+  "OR10-02": datetime.datetime(2010, 5, 14),
+  "A10-01": datetime.datetime(2009, 11, 6),
+  "A10-01A": datetime.datetime(2010, 3, 2),
+  "OR10-01": datetime.datetime(2010, 3, 4),
+  "A10-02": datetime.datetime(2010, 1, 28),
+  "A10-03": datetime.datetime(2010, 3, 1),
+  "A09-01": datetime.datetime(2008, 11, 6),
+  "A09-01A": datetime.datetime(2009, 1, 15),
+  "OR09-01": datetime.datetime(2009, 1, 12),
+  "A09-02": datetime.datetime(2009, 2, 6),
+  "A09-03": datetime.datetime(2009, 7, 7),
+  "A09-04": datetime.datetime(2009, 6, 30),
+  "A09-05": datetime.datetime(2009, 7, 20),
+  "A09-06": datetime.datetime(2009, 7, 28),
+  "A09-07": datetime.datetime(2009, 8, 21),
+  "A08-01": datetime.datetime(2007, 11, 16),
+  "A08-02": datetime.datetime(2007, 11, 6),
+  "A08-02A": datetime.datetime(2007, 12, 12),
+  "A08-03": datetime.datetime(2008, 1, 23),
+  "A08-04": datetime.datetime(2008, 3, 18),
+  "A08-05": datetime.datetime(2008, 8, 29),
+  "A08-06": datetime.datetime(2008, 9, 10),
+  "A08-07": datetime.datetime(2008, 9, 22),
+  "A08-08": datetime.datetime(2008, 9, 22),
+  "A07-01": datetime.datetime(2006, 11, 13),
+  "OR07-01": datetime.datetime(2007, 1, 19),
+  "A07-02": datetime.datetime(2007, 5, 4),
+  "OR07-02": datetime.datetime(2007, 6, 29),
+  "A06-01": datetime.datetime(2006, 3, 30),
+  "OR06-01": datetime.datetime(2006, 8, 22),
+  "A06-02": datetime.datetime(2006, 8, 1),
+  "A06-04": datetime.datetime(2006, 10, 2),
+}
 
 def run(options):
   year_range = inspector.year_range(options, archive)
@@ -104,16 +167,18 @@ def report_from(result, landing_url, report_type, year_range):
       report_filename = report_url.split("/")[-1]
       report_id, _ = os.path.splitext(report_filename)
 
-  estimated_date = False
+  published_on = None
+  if report_id in REPORT_PUBLISHED_MAP:
+    published_on = REPORT_PUBLISHED_MAP[report_id]
+  if not published_on:
+    try:
+      published_on_text = title.split("-")[-1].strip()
+      published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
+    except ValueError:
+      pass
 
-  try:
-    published_on_text = title.split("-")[-1].strip()
-    published_on = datetime.datetime.strptime(published_on_text, '%B %d, %Y')
-  except ValueError:
-    # For reports where we can only find the year, set them to Nov 1st of that year
-    published_on_year = re.findall('\d+', report_id)[0]
-    published_on = datetime.datetime.strptime("November 1, {}".format(published_on_year), '%B %d, %y')
-    estimated_date = True
+  if not published_on:
+    raise inspector.NoDateFoundError(report_id, title)
 
   if published_on.year not in year_range:
     logging.debug("[%s] Skipping, not in requested range." % report_url)
@@ -130,8 +195,6 @@ def report_from(result, landing_url, report_type, year_range):
     'title': title,
     'published_on': datetime.datetime.strftime(published_on, "%Y-%m-%d"),
   }
-  if estimated_date:
-    report['estimated_date'] = estimated_date
   return report
 
 utils.run(run) if (__name__ == "__main__") else None
