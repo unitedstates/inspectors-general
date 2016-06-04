6908e09f2b37f8b3f8fc05ebc961a9f59fa598eb
David Cook <divergentdave@gmail.com>
2016-06-03 19:36:11 -0500

    [cftc] Add date

--- inspectors/cftc.py
+++ inspectors/cftc.py
@@ -23,6 +23,7 @@ REPORT_PUBLISHED_MAPPING = {
   "oigocoaudit2014": datetime.datetime(2014, 5, 1),
   "oigcommentletter042214": datetime.datetime(2014, 4, 22),
   "oig_auditruleenforcementreview": datetime.datetime(2015, 8, 5),
+  "oig_ipera2015": datetime.datetime(2016, 5, 16),
   "oig_pensionawardsaudit": datetime.datetime(2015, 9, 24),
   "oigfinancialstatementalert": datetime.datetime(2016, 1, 15),
   "2015finstatementaudit": datetime.datetime(2016, 1, 15),
