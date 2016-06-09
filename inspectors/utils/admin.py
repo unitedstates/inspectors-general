f56402a7dd81d58e7154d1c980cbc187fe7af1ab
David Cook <divergentdave@gmail.com>
2016-06-08 21:52:52 -0500

    Refactor error handling, start work on dashboard

--- inspectors/utils/admin.py
+++ inspectors/utils/admin.py
@@ -6,6 +6,7 @@ import traceback
 import yaml
 import logging
 import re
+import atexit
 
 import smtplib
 import email.utils
@@ -15,94 +16,58 @@ import json
 import urllib.request
 import urllib.parse
 
-# read in an opt-in config file for changing directories and supplying email settings
+# read in an opt-in config file for changing directories and supplying settings
 # returns None if it's not there, and this should always be handled gracefully
 path = "admin.yml"
 if os.path.exists(path):
-    config = yaml.load(open(path))
+  config = yaml.load(open(path))
 else:
-    config = None
+  config = None
 
 
 def notify(body):
+  for error_handler in error_handlers:
     try:
-        if isinstance(body, Exception):
-            body = format_exception(body)
-
-        logging.error(body)  # always print it
-
-        if config:
-            details = config.get('email')
-            if details:
-                send_email(body)
-
-            if config.get('slack'):
-                send_slack(body)
-
+      if isinstance(body, Exception):
+        body = format_exception(body)
+      error_handler.log(body)
     except Exception as exception:
-        print("Exception logging message to admin, halting as to avoid loop")
-        print(format_exception(exception))
+      print("Exception logging message to admin, halting as to avoid loop")
+      print(format_exception(exception))
 
 
 def format_exception(exception):
-    exc_type, exc_value, exc_traceback = sys.exc_info()
-    return "\n".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+  exc_type, exc_value, exc_traceback = sys.exc_info()
+  return "\n".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
 
-# this should only be called if the settings are definitely there
-
-
-def send_email(message):
-    settings = config['email']
-
-    # adapted from http://www.doughellmann.com/PyMOTW/smtplib/
-    msg = MIMEText(message)
-    msg.set_unixfrom('author')
-    msg['To'] = email.utils.formataddr(('Recipient', settings['to']))
-    msg['From'] = email.utils.formataddr((settings['from_name'], settings['from']))
-    msg['Subject'] = settings['subject']
-
-    server = smtplib.SMTP(settings['hostname'])
-    try:
-        server.ehlo()
-        if settings['starttls'] and server.has_extn('STARTTLS'):
-            server.starttls()
-            server.ehlo()
-
-        server.login(settings['user_name'], settings['password'])
-        server.sendmail(settings['from'], [settings['to']], msg.as_string())
-    finally:
-        server.quit()
-
-    logging.info("Sent email to %s" % settings['to'])
 
 def copy_if_present(key, src, dst):
-    if key in src:
-        dst[key] = src[key]
+  if key in src:
+    dst[key] = src[key]
 
 INSPECTOR_RE = re.compile('''File "\\.?inspectors(?:/|\\\\)([a-z]+)\\.py", line ([0-9]+), in ([^\n]+)\n''')
 HTTP_ERROR_RE = re.compile('''scrapelib\\.HTTPError: ([0-9]+) while retrieving ([^\n]+)\n''')
 TRACEBACK_STR = "Traceback (most recent call last):"
 
-def send_slack(body):
-    options = config["slack"]
-    webhook_url = options["webhook"]
-
-    body = str(body)
+class ErrorHandler(object):
+  def log(self, body):
+    if not self.options:
+      return
     lines = body.split('\n')
 
     # Scan up to the point where the traceback starts
     index = 0
     while index < len(lines) and lines[index].find(TRACEBACK_STR) != -1:
-        index = index + 1
+      index = index + 1
     # Skip the traceback
     index = index + 1
     # Scan until the first non-indented line, after the traceback
     while index < len(lines) and (lines[index] == "" or lines[index][0] == " "):
-        index = index + 1
+      index = index + 1
     # If we ran off the end of the message because it's in an unexpected
     # format, just use the whole thing
     if index >= len(lines):
-        index = 0
+      index = 0
     # Take the remaining text as the exception message
     exception_message = '\n'.join(lines[index:])
     # Use one line only of the exception message as a fallback message for
@@ -112,72 +77,170 @@ def send_slack(body):
     # The exception type should be the first thing after the traceback,
     # followed by a colon
     if exception_message.find(':') != -1:
-        class_name = exception_message[:exception_message.find(':')]
+      class_name = exception_message[:exception_message.find(':')]
     else:
-        class_name = None
+      class_name = None
 
     scraper = None
     line_num = None
     function = None
     scraper_matches = INSPECTOR_RE.findall(body)
     if scraper_matches:
-        scraper = scraper_matches[-1][0]
-        line_num = scraper_matches[-1][1]
-        function = scraper_matches[-1][2]
+      scraper = scraper_matches[-1][0]
+      line_num = scraper_matches[-1][1]
+      function = scraper_matches[-1][2]
 
     http_error_match = HTTP_ERROR_RE.search(body)
-
     if http_error_match:
-        pretext = "%s error while downloading %s" % \
-                  (http_error_match.group(1), http_error_match.group(2))
-        message = {
-            "attachments": [
-                {
-                    "fallback": fallback,
-                    "text": body,
-                    "color": "warning",
-                    "pretext": pretext
-                }
-            ]
-        }
+      self.log_http_error(http_error_match.group(1), http_error_match.group(2),
+                          fallback, body)
     elif class_name and scraper and line_num and function:
-        pretext = ("%s was thrown while running %s.py " + \
-                  "(line %s, in function %s)") % \
-                  (class_name, scraper, line_num, function)
-        message = {
-            "attachments": [
-                {
-                    "fallback": fallback,
-                    "text": body,
-                    "color": "danger",
-                    "pretext": pretext
-                }
-            ]
-        }
+      self.log_exception(class_name, scraper, line_num, function,
+                         fallback, body)
     elif body.startswith("QA results for"):
-        message = {
-            "attachments": [
-                {
-                    "fallback": fallback,
-                    "text": "\n".join(lines[1:]),
-                    "color": "danger",
-                    "pretext": fallback
-                }
-            ]
-        }
+      self.log_qa("\n".join(lines[1:]), fallback)
     else:
-        message = {
-            "text": str(body)
-        }
+      self.log_fallback(str(body))
 
-    copy_if_present("username", options, message)
-    copy_if_present("icon_url", options, message)
-    copy_if_present("icon_emoji", options, message)
-    copy_if_present("channel", options, message)
+class ConsoleErrorHandler(ErrorHandler):
+  def log(self, body):
+    logging.error(body)
+
+class EmailErrorHandler(ErrorHandler):
+  def log(self, body):
+    settings = config['email']
+
+    # adapted from http://www.doughellmann.com/PyMOTW/smtplib/
+    msg = MIMEText(body)
+    msg.set_unixfrom('author')
+    msg['To'] = email.utils.formataddr(('Recipient', settings['to']))
+    msg['From'] = email.utils.formataddr((settings['from_name'], settings['from']))
+    msg['Subject'] = settings['subject']
+
+    server = smtplib.SMTP(settings['hostname'])
+    try:
+      server.ehlo()
+      if settings['starttls'] and server.has_extn('STARTTLS'):
+        server.starttls()
+        server.ehlo()
+
+      server.login(settings['user_name'], settings['password'])
+      server.sendmail(settings['from'], [settings['to']], msg.as_string())
+    finally:
+      server.quit()
+
+    logging.info("Sent email to %s" % settings['to'])
+
+class SlackErrorHandler(ErrorHandler):
+  def __init__(self):
+    self.options = config.get("slack")
+
+  def send_message(self, message):
+    copy_if_present("username", self.options, message)
+    copy_if_present("icon_url", self.options, message)
+    copy_if_present("icon_emoji", self.options, message)
+    copy_if_present("channel", self.options, message)
 
     message_json = json.dumps(message)
-    message_bytes = message_json.encode('utf-8')
+    message_bytes = message_json.encode("utf-8")
 
-    request = urllib.request.Request(webhook_url, message_bytes)
-    request.add_header('Content-Type', 'application/json; charset=utf-8')
+    request = urllib.request.Request(self.options["webhook"], message_bytes)
+    request.add_header("Content-Type", "application/json; charset=utf-8")
     urllib.request.urlopen(request)
+
+  def log_http_error(self, http_status_code, url, fallback, body):
+    pretext = "%s error while downloading %s" % (http_status_code, url)
+    self.send_message({
+      "attachments": [
+        {
+          "fallback": fallback,
+          "text": body,
+          "color": "warning",
+          "pretext": pretext
+        }
+      ]
+    })
+
+  def log_exception(self, class_name, scraper, line_num, function,
+                    fallback, body):
+    pretext = ("%s was thrown while running %s.py (line %s, in function %s)" %
+               (class_name, scraper, line_num, function))
+    self.send_message({
+      "attachments": [
+        {
+          "fallback": fallback,
+          "text": body,
+          "color": "danger",
+          "pretext": pretext
+        }
+      ]
+    })
+
+  def log_qa(self, text, fallback):
+    self.send_message({
+      "attachments": [
+        {
+          "fallback": fallback,
+          "text": text,
+          "color": "danger",
+          "pretext": fallback
+        }
+      ]
+    })
+
+  def log_fallback(self, body):
+    self.send_message({
+      "text": str(body)
+    })
+
+
+class DashboardErrorHandler(ErrorHandler):
+  def __init__(self):
+    self.options = config.get("dashboard")
+    self.dashboard_data = {}
+    atexit.register(self.dashboard_send)
+
+  def log_http_error(self, http_status_code, url, fallback, body):
+    pass  # self.dashboard_data[todo]
+
+  def log_exception(self, class_name, scraper, line_num, function,
+                    fallback, body):
+    if scraper not in self.dashboard_data:
+      self.dashboard_data[scraper] = {}
+    if "exceptions" not in self.dashboard_data[scraper]:
+      self.dashboard_data[scraper]["exceptions"] = []
+    entry = {
+      "class_name": class_name,
+      "filename": "inspectors/%.py" % scraper,
+      "line_num": line_num,
+      "function": function
+    }
+    self.dashboard_data[scraper]["exceptions"].append(entry)
+
+  def log_qa(self, text, fallback):
+    pass
+
+  def log_fallback(self, body):
+    pass
+
+  def dashboard_send(self):
+    if not self.dashboard_data:
+      return
+
+    options = config["dashboard"]
+    message_json = json.dumps(self.dashboard_data)
+    message_bytes = message_json.encode("utf-8")
+    url = options["url"] + "?secret=" + urllib.parse.quote(options["secret"])
+    request = urllib.request.Request(url, message_bytes)
+    request.add_header("Content-Type", "application/json; charset=utf-8")
+    request.get_method = lambda: "PUT"
+    urllib.request.urlopen(request)
+
+error_handlers = [ConsoleErrorHandler()]
+if config:
+  if config.get("email"):
+    error_handlers.append(EmailErrorHandler())
+  if config.get("slack"):
+    error_handlers.append(SlackErrorHandler())
+  if config.get("dashboard"):
+    error_handlers.append(DashboardErrorHandler())
