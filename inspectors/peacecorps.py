7eacd72f9f23de3a716b8052a4908d16a6facc2d
David Cook <divergentdave@gmail.com>
2016-12-02 16:09:56 -0600

    [peacecorps] Add date

--- inspectors/peacecorps.py
+++ inspectors/peacecorps.py
@@ -200,6 +200,7 @@ REPORT_PUBLISHED_MAPPING = {
   "Final_Audit_of_Peace_Corps_Colombia_IG-16-05-A_7OVJYcv": datetime.datetime(2016, 9, 23),
   "Recurring_Issues_Report": datetime.datetime(2016, 9, 23),
   "OIG_Annual_Plan_FY17": datetime.datetime(2016, 9, 30),
+  "2016_Peace_Corps_Management_and_Performance_Challenges": datetime.datetime(2016, 11, 30),
   "FISMA_final_report": datetime.datetime(2016, 11, 10),
   "406_highlights": datetime.datetime(2016, 8, 1),
   "Final_Evaluation_Report_on_the_Peace_Corps_Sexual_Assault_Risk_Reduction_and_Response_Program": datetime.datetime(2016, 11, 28),
