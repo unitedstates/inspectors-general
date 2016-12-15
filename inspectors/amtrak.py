983edf12f1b399dfa8b05da67cbb6cd24a54979d
David Cook <divergentdave@gmail.com>
2016-12-15 05:15:26 -0600

    [amtrak] Handle more reused report numbers

--- inspectors/amtrak.py
+++ inspectors/amtrak.py
@@ -66,9 +66,11 @@ def report_from(result):
       report_id = "008-2015-challenges"
     else:
       report_id = "008-2015-msa"
-  if "/OIG-I-2016-517_0.pdf" in url or "/OIG-I-2016-525_0.pdf" in url:
-    # Second of two investigative summaries under this tracking number
-    report_id += "-2"
+  for filename in ("OIG-I-2016-517_0.pdf", "OIG-I-2016-525_0.pdf",
+                   "OIG-I-2016-529_0.pdf", "OIG-I-2016-501_0.pdf"):
+    if url.endswith(filename):
+      # Second of two investigative summaries under this tracking number
+      report_id += "-2"
 
   if not report_id:
     report_id = url[url.rfind('/') + 1 : url.rfind('.')]
